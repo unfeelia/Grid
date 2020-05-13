17f44cebd33099c485afda35681c2e3d63b8d9ae
syusupov <sergey.yusupov@dataart.com>
2020-05-13 12:05:29 +0300

    Initial commit

---
+++ GridAndCloud/GridAndCloud/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GridAndCloud.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
