4164ca480bd0fbdf2dc9ac750b7dc8f4680f924b
syusupov <sergey.yusupov@dataart.com>
2020-05-15 15:21:39 +0300

    added main controller actions and filterModel

--- GridAndCloud/GridAndCloud.Web/Controllers/HomeController.cs
+++ GridAndCloud/GridAndCloud.Web/Controllers/HomeController.cs
@@ -6,32 +6,57 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using GridAndCloud.Web.Models;
+using GridAndCloud.BusinessLogic;
+using GridAndCloud.CoreModels;
+using GridAndCloud.CoreModels.Filters;
 
 namespace GridAndCloud.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IElementRepository _repository;
+        private readonly ILogic _logic;
+        private readonly IClassterisater _classterisater;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IElementRepository repository,
+            IClassterisater classterisater,
+            ILogic logic)
         {
             _logger = logger;
+            _repository = repository;
+            _classterisater = classterisater;
+            _logic = logic;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(GetModel());
         }
 
-        public IActionResult Privacy()
+        public IActionResult ClustersByFilters(FilterModel[] filters)
         {
-            return View();
+            var realFilter = filters.Select(x => x.GetFilter());
+            var items = _repository.Get(realFilter);
+
+            var clusters = _classterisater.ClasteriseElements(items, 4);
+
+            return View("Clusters", clusters);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult ClustersByIds(int[] ids)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var items = _repository.Get(ids);
+
+            var clusters = _classterisater.ClasteriseElements(items, 4);
+
+            return View("Clusters", clusters);
+        }
+
+        private Model GetModel()
+        {
+            return _logic.GetElementModel();
         }
     }
 }
---
+++ GridAndCloud/GridAndCloud.Web/Models/FilterModel.cs
@@ -0,0 +1,53 @@
+﻿using GridAndCloud.CoreModels.Filters;
+using System;
+using System.Collections.Generic;
+
+namespace GridAndCloud.Web.Models
+{
+    public enum FilterType { Filter, CompareWithFilter, InBetweenFilter, OneOfFilter }
+
+    public class FilterModel
+    {
+        public int AttributeId { get; set; }
+
+        public virtual Filter GetFilter()
+        {
+            return new Filter(AttributeId);
+        }
+    }
+
+    public sealed class CompareWithFilterModel : FilterModel
+    {
+        public ValueType Bound { get; set; }
+        public bool Inclusive { get; set; }
+        public bool GreaterThan { get; set; }
+
+        public override Filter GetFilter()
+        {
+            return new CompareWithFilter(AttributeId, Bound, GreaterThan, Inclusive);
+        }
+    }
+
+    public sealed class InBetweenFilterModel : FilterModel
+    {
+        public ValueType LowerBound { get; set; }
+        public ValueType UpperBound { get; set; }
+        public bool IncludeLowerBound { get; set; }
+        public bool IncludeUpperBound { get; set; }
+
+        public override Filter GetFilter()
+        {
+            return new InBetweenFilter(AttributeId, LowerBound, UpperBound, IncludeLowerBound, IncludeUpperBound);
+        }
+    }
+
+    public sealed class OneOfFilterModel : FilterModel
+    {
+        public IEnumerable<ValueType> Options { get; set; }
+
+        public override Filter GetFilter()
+        {
+            return new OneOfFilter(AttributeId, Options);
+        }
+    }
+}
