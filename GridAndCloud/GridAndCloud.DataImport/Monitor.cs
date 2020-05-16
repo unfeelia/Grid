863af3ce16f4d4dbdb879521b01989cea8dab3c3
syusupov <sergey.yusupov@dataart.com>
2020-05-16 03:10:12 +0300

    data import project

---
+++ GridAndCloud/GridAndCloud.DataImport/Monitor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GridAndCloud.DataImport
+{
+    [Serializable]
+    public class Monitor
+    {
+        public string ItemName { get; set; }
+        public string Price { get; set; }
+        public string Provider { get; set; }
+        public string Photos { get; set; }
+        public string PhotosLinks { get; set; }
+        public string Characteristics { get; set; }
+        public string Color { get; set; }
+
+        public Dictionary<string, string> chars 
+        { 
+            get 
+            {
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(Characteristics);
+
+                return doc.DocumentNode.SelectSingleNode("//table")
+                            .Descendants("tr")
+                            .Skip(1)
+                            .Where(tr => tr.Elements("td").Count() > 1)
+                            .ToDictionary(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).First(), tr => tr.Elements("td").Select(td => td.InnerText.Trim()).Last());
+            } 
+        }
+    }
+}
---
+++ GridAndCloud/GridAndCloud.DataImport/Program.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridAndCloud.DataImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"");
+            List<Monitor> obj = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Monitor>>(text).Where(x => x.Color == null).ToList();
+
+            string server = "";
+            string database = "";
+            string uid = "";
+            string password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            var sb = new StringBuilder();
+
+            sb.Append("INSERT INTO Monitor(ItemName, Price, Provider, Photos, PhotosLinks, Characteristics, Color) VALUES ");
+
+            foreach (var monitor in obj)
+            {
+                sb.Append("('" + monitor.ItemName.Trim().Replace("'", "") + 
+                    "', '" + monitor.Price.Trim().Replace("'", "") + 
+                    "', '" + monitor.Provider.Trim().Replace("'", "") + 
+                    "', '" + monitor.Photos.Trim().Replace("'", "") + 
+                    "', '" + monitor.PhotosLinks.Trim().Replace("'", "") + 
+                    "', '" + monitor.Characteristics.Trim().Replace("'", "") + 
+                    "', '" + monitor.Color + "'),");
+            }
+
+            File.WriteAllText(@"", sb.ToString());
+        }
+    }
+}
