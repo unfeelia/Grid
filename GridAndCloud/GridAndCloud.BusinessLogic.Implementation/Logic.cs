366eba57046310ba0b60af3536a801678817743a
syusupov <sergey.yusupov@dataart.com>
2020-05-16 03:09:38 +0300

    logic

--- GridAndCloud/GridAndCloud.BusinessLogic.Implementation/Logic.cs
+++ GridAndCloud/GridAndCloud.BusinessLogic.Implementation/Logic.cs
@@ -1,7 +1,8 @@
 ﻿using GridAndCloud.CoreModels;
+using GridAndCloud.CoreModels.Filters;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Attribute = GridAndCloud.CoreModels.Attributes.Attribute;
 
 namespace GridAndCloud.BusinessLogic.Implementation
 {
@@ -9,7 +10,23 @@ namespace GridAndCloud.BusinessLogic.Implementation
     {
         public Model GetElementModel()
         {
+            return new Model(GetAttributes());
+        }
+
+        public IEnumerable<Attribute> GetAttributes()
+        {
             throw new NotImplementedException();
+        }
+
+        public IEnumerable<Filter> GetFilters()
+        {
+            return new Filter[] 
+            { 
+                new Filter(new Attribute(1, new int[]{ 1, 2, 4, 5, 9, 11 }, (x, y) => x - y, 10, 0, (x) => x.ToString())),
+                new OneOfFilter(new Attribute(2, new int[]{ 1, 2, 4, 5, 9, 11 }, (x, y) => x - y, 10, 0, (x) => x.ToString()), new int[] { 1, 2, 123 }),
+                new CompareWithFilter(new Attribute(3, new int[]{ 1, 2, 4, 5, 9, 11 }, (x, y) => x - y, 10, 0, (x) => x.ToString()), 3),
+                new InBetweenFilter(new Attribute(4, new int[]{ 1, 2, 4, 5, 9, 11 }, (x, y) => x - y, 10, 0, (x) => x.ToString()), 3, 6)
+            };
         }
     }
 }
--- GridAndCloud/GridAndCloud.BusinessLogic/ILogic.cs
+++ GridAndCloud/GridAndCloud.BusinessLogic/ILogic.cs
@@ -1,9 +1,12 @@
 ﻿using GridAndCloud.CoreModels;
+using GridAndCloud.CoreModels.Filters;
+using System.Collections.Generic;
 
 namespace GridAndCloud.BusinessLogic
 {
     public interface ILogic
     {
         Model GetElementModel();
+        IEnumerable<Filter> GetFilters();
     }
 }
